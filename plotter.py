6f381373fe6d328c54a23186a32b7d1300a9c0d5
bjolicoeur10 <110859440+bjolicoeur10@users.noreply.github.com>
2023-10-26 00:46:36 -0500

    plotter.py
    Imports newer file type

--- plotter.py
+++ plotter.py
@@ -1,16 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.io import loadmat
+import h5py
 from matplotlib.animation import FuncAnimation
 
-# Load the data
-data = loadmat('Bc.mat')
-Bc = data['Bc']
+# Open the MATLAB v7.3 file using h5py
+with h5py.File('Bc.mat', 'r') as file:
+    Bc = file['Bc'][:]  # Load the data
+
+# Reshape the data to the desired shape (11x11x11x60)
+Bc = Bc.transpose(3, 2, 1, 0)
 
 # Get the shape of the Bc array
 array_shape = Bc.shape
-
+print(array_shape)
 # Create a meshgrid for x, y, and z based on the array shape
 x, y, z = np.meshgrid(np.arange(array_shape[0]), np.arange(array_shape[1]), np.arange(array_shape[2]))
 
