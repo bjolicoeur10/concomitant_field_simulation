41be66712253216a1959c1763baaa2826c974bd0
bjolicoeur10 <110859440+bjolicoeur10@users.noreply.github.com>
2023-10-31 21:08:18 -0500

    Update getgrads.m

--- getgrads.m
+++ getgrads.m
@@ -1,20 +1,39 @@
 B0 = 3000; % 3000 mT = 3T
-Bmax = 0.2; % mT/mm
-ele = 22;
+ele = 220;
 ni = ele/2;
 t = 1:120; %1t = 0.01ms -> 1000t = 10 ms
+index = 1;
 for ti = 1:numel(t)
     ti
-    for xi = -ni:ni
-        for yi = -ni:ni
-            for zi = -ni:ni
-                Bc(xi+ni+1, yi+ni+1, zi+ni+1, ti) = (1/2*B0) * (((Gx(ti) * zi - Gz(ti) * ...
+    for xi = -ni:index:ni
+        for yi = -ni:index:ni
+            for zi = -ni:index:ni
+                Bc(xi+ni+1, yi+ni+1, zi+ni+1, ti) = 267.513 * (1/(2*B0)) * (((Gx(ti) * zi - Gz(ti) * ...
                     zi/2)^2 + (Gy(ti) * zi - Gz(ti) * yi/2)^2));
             end
         end
     end
 end
+x = ele+1;
+y = ele+1;
+z = ele+1;
+t = numel(t);
 
-save('Bc.mat','Bc','-v7.3')
-clear all
-close all
+% Create a new 4D matrix to store the cumulative sums
+[x, y, z, t] = size(Bc);
+Bcsum = zeros(x, y, z, t);
+
+% Initialize the first frame of Bcsum with the first frame of Bc
+Bcsum(:, :, :, 1) = Bc(:, :, :, 1);
+
+% Compute the cumulative sum for the rest of the frames
+for t_idx = 2:t
+    Bcsum(:, :, :, t_idx) = Bcsum(:, :, :, t_idx - 1) + Bc(:, :, :, t_idx);
+end
+
+% gammabar = 42.576 gamma / 2pi [MHz * T^-1]
+% save('Bc.mat','Bc','-v7.3')
+% 
+% 
+% clear all
+% close all
